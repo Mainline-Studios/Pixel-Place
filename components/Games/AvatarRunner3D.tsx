b7247453e7ec7bfbcdc9769f5317ef5eff7f63c7
Brennan Kelly <brennankelly@Mac-11551.lan>
2025-12-11 15:09:45 -0600

    Fix PasswordGate syntax and create UserMadeGamePlayer component

--- components/Games/AvatarRunner3D.tsx
+++ components/Games/AvatarRunner3D.tsx
@@ -328,3 +328,4 @@ export default function AvatarRunner3D({ user, onClose }: AvatarRunner3DProps) {
 
 
 
+
--- components/Games/SnakeGame.tsx
+++ components/Games/SnakeGame.tsx
@@ -200,3 +200,4 @@ export default function SnakeGame({ onClose }: SnakeGameProps) {
 
 
 
+
